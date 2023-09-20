390ce5b6115728d3ecc3dc79ca47542e2b74e826
svidzger <german.svidzinskij@gmail.com>
2023-09-20 14:57:39 +0300

    models user

--- niksari/models.py
+++ niksari/models.py
@@ -1,20 +1,28 @@
 from django.db import models
 
-# Create your models here.
 
+# Create your models here.
 class Furniture(models.Model):
     category = models.ForeignKey()
+
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
     type = category = models.ForeignKey()
+
 
 class Type(models.Model):
     type_name = models.CharField(max_length=50)
     model = models.ForeignKey()
     indoor = models.BooleanField(default=True)
 
+
 class Model(models.Model):
     name = models.CharField(max_length=50)
     material = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
