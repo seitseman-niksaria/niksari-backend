d8526a12bd2d37b2cd0bbf04b6fef212dfbdf241
SProkopios <s.koponen93@gmail.com>
2023-09-20 16:41:57 +0300

    classes

--- niksari/models.py
+++ niksari/models.py
@@ -2,19 +2,26 @@ from django.db import models
 
 # Create your models here.
 
-class Furniture(models.Model):
-    category = models.ForeignKey()
+# class Furniture(models.Model):
+#     category = models.ForeignKey()
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
-    type = category = models.ForeignKey()
 
-class Type(models.Model):
-    type_name = models.CharField(max_length=50)
-    model = models.ForeignKey()
-    indoor = models.BooleanField(default=True)
+#class Type(models.Model):
+#    type_name = models.CharField(max_length=50)
+#    model = models.ForeignKey()
+#    indoor = models.BooleanField(default=True)
 
 class Model(models.Model):
     name = models.CharField(max_length=50)
     material = models.CharField(max_length=50)
     description = models.CharField(max_length=50)
+    category_id = models.ForeignKey("")
+    material_id = models.ForeignKey()
+
+class MaterialOfModel(models.Model):()
+
+class Material(models.Model):
+    name = models.CharField(max_length=50)
+    material
