7408d5445d1f1d0a21c3767526a96439e5d80c10
svidzger <german.svidzinskij@gmail.com>
2023-09-20 15:01:58 +0300

    psycopg2 settings

--- backend/settings.py
+++ backend/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": 'django.db.backends.postgresql_psycopg2',
         "NAME": os.environ.get('DB_NAME'),
         "USER": os.environ.get('DB_USER'),
         "PASSWORD": os.environ.get('DB_PASSWORD'),
