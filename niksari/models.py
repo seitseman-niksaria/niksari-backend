f598bf9ee153a3e6397c750a0abcff21503fe887
svidzger <german.svidzinskij@gmail.com>
2023-09-25 13:48:38 +0300

    Django models #1

--- niksari/models.py
+++ niksari/models.py
@@ -1,13 +1,33 @@
 from django.db import models
 
 
-# Create your models here.
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
 
 
+class Upholstery(models.Model):
+    upholstery_name = models.CharField(max_length=50)
+
+
+class Material(models.Model):
+    material_name = models.CharField(max_length=50)
+
+
+class SurfaceFinish(models.Model):
+    surface_finish_name = models.CharField(max_length=50)
+
+
 class FurnitureModel(models.Model):
-    name = models.CharField(max_length=50)
-    material = models.CharField(max_length=50)
-    description = models.CharField(max_length=50)
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    furniture_name = models.CharField(max_length=50)
+    furniture_description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    surface_finish = models.ForeignKey(SurfaceFinish, on_delete=models.CASCADE)
+    upholstery = models.ForeignKey(Upholstery, on_delete=models.CASCADE)
+
+
+class Instruction(models.Model):
+    instruction_text = models.TextField()
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    surface_finish = models.ForeignKey(SurfaceFinish, on_delete=models.CASCADE)
+    upholstery = models.ForeignKey(Upholstery, on_delete=models.CASCADE)
