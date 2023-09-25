46efb1a558b0bad259a8e969556d578c4e862330
svidzger <german.svidzinskij@gmail.com>
2023-09-25 16:06:34 +0300

    niksari category table modifiable in the django admin

--- niksari/models.py
+++ niksari/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.category_name
 
 
 class Upholstery(models.Model):
