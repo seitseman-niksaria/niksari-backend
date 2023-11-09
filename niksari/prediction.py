5f5f098ef26f41c2d6dd727ae4c5cd1f70ddbadf
SProkopios <s.koponen93@gmail.com>
2023-11-09 20:18:46 +0200

    Improving prediction.py and REST

--- niksari/prediction.py
+++ niksari/prediction.py
@@ -1,7 +1,7 @@
 from keras.models import load_model  # TensorFlow is required for Keras to work
 from teachable_machine import TeachableMachine
 import numpy as np
-
+from .models import FurnitureModel
 
 teachable_model = TeachableMachine(model_path="teachable_machine\keras_model.h5", labels_file_path="teachable_machine\labels.txt")
 
@@ -9,22 +9,12 @@ teachable_model = TeachableMachine(model_path="teachable_machine\keras_model.h5"
 def predict_image(user_image):
 
     result = teachable_model.classify_image(user_image)
+    db_index = getIndex(result["class_index"]) # calling getIndex
+    model = FurnitureModel.objects.values().get(pk=db_index) #Getting data from database with index
+    return model
 
-    print("class_index", result["class_index"])
-
-    print("class_name:", result["class_name"])
-
-    print("class_confidence:", result["class_confidence"])
-
-    print("predictions:", result["predictions"])
-
-    
-    class_name = result["class_name"]
-    class_index = result["class_index"]
-    class_confidence = result["class_confidence"]
-
-    return {
-        "class_name": class_name,
-        "class_index": class_index,
-        "class_confidence": class_confidence
-    }
+# Changin the "class_index" to corresponding database index
+def getIndex(class_index):
+    newIndex = {0:1, 1:24, 2:48}
+    newValue = newIndex.get(class_index)
+    return newValue
