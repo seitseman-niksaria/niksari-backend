cdd04083704b3dea2ab6d221a36463b89345f5a3
SProkopios <s.koponen93@gmail.com>
2023-11-27 15:06:02 +0200

    Fixing bugs

--- backend/settings.py
+++ backend/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL_POSTGRE"))
 }
 
 
